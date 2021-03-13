b6608f258aba453a60f7b2d48280fb326bc2459a
Mateus Paiva <64433509+mampaiva@users.noreply.github.com>
2021-03-13 13:04:35 -0300

    Update fix-burai.sh

--- fix-burai.sh
+++ fix-burai.sh
@@ -5,7 +5,8 @@
 
 echo "deb http://old-releases.ubuntu.com/ubuntu/ artful main restricted universe multiverse" >> /etc/apt/sources.list #add the ubuntu 17.10 repositories
 
-apt update 
+apt update --allow-unauthenticated
+apt update --allow-insecure-repositories
 
 echo "Package:  *openjfx*  
 Pin: release n=artful
@@ -15,7 +16,7 @@ Package:  *libopenjfx*
 Pin: release n=artful
 Pin-Priority: 1001   
 
-Package:  *openjdk-8-jdk*  
+Package:  *openjdk-8*  
 Pin: release n=artful
 Pin-Priority: 1001   " >> /etc/apt/preferences.d/openjfx # change the priority of package versions
 
